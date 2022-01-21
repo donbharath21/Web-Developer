0506d5dd0393b4cf32d7569da2dd5e4d3bea48f7
donbharath21 <donbharath21@gmail.com>
2022-01-21 15:07:02 +0530

    first commit

---
+++ DATABASE/Sqlfile(php).sql
@@ -0,0 +1,381 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2020 at 07:05 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `itphp14`
+--
+create database `itphp14`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_power`
+--
+
+CREATE TABLE `add_power` (
+  `image` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `latitude` varchar(100) NOT NULL,
+  `longitude` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `des` text NOT NULL,
+  `id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `add_power`
+--
+
+INSERT INTO `add_power` (`image`, `name`, `phone`, `latitude`, `longitude`, `address`, `des`, `id`) VALUES
+('join query.PNG', 'dsfsdf', '234324242', '13.036516', ' 80.230782', 't-nagar', 'chennai', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_product`
+--
+
+CREATE TABLE `add_product` (
+  `id` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `milage` varchar(100) NOT NULL,
+  `fuel_type` varchar(100) NOT NULL,
+  `engile_cc` varchar(100) NOT NULL,
+  `torque` varchar(100) NOT NULL,
+  `seat_capacity` varchar(100) NOT NULL,
+  `transmission_type` varchar(100) NOT NULL,
+  `boot_space` varchar(100) NOT NULL,
+  `fuel_tank_capacity` varchar(100) NOT NULL,
+  `body_type` varchar(100) NOT NULL,
+  `service_cost` varchar(100) NOT NULL,
+  `power_steering` varchar(100) NOT NULL,
+  `power_window` varchar(100) NOT NULL,
+  `ac` varchar(100) NOT NULL,
+  `driver_airbag` varchar(100) NOT NULL,
+  `passenger_airbag` varchar(100) NOT NULL,
+  `alloy_wheel` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `add_spare`
+--
+
+CREATE TABLE `add_spare` (
+  `id` int(100) NOT NULL,
+  `file` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `vehicle_type` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `des` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(40) NOT NULL,
+  `emp_id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `joining_date` varchar(100) NOT NULL,
+  `uname` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enquiry`
+--
+
+CREATE TABLE `enquiry` (
+  `id` int(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `booking_date` varchar(100) NOT NULL,
+  `product_id` varchar(100) NOT NULL,
+  `enquiry_date` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(100) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loan`
+--
+
+CREATE TABLE `loan` (
+  `id` int(100) NOT NULL,
+  `cus_inc` varchar(100) NOT NULL,
+  `car_price` varchar(100) NOT NULL,
+  `amt` varchar(100) NOT NULL,
+  `des` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `uname` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `service`
+--
+
+CREATE TABLE `service` (
+  `id` int(100) NOT NULL,
+  `vehicle_name` varchar(100) NOT NULL,
+  `vehicle_type` varchar(100) NOT NULL,
+  `vehicle_year` varchar(100) NOT NULL,
+  `problem` text NOT NULL,
+  `delivery_date` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `post_date` varchar(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_product`
+--
+
+CREATE TABLE `user_product` (
+  `id` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `milage` varchar(100) NOT NULL,
+  `fuel_type` varchar(100) NOT NULL,
+  `engile_cc` varchar(100) NOT NULL,
+  `torque` varchar(100) NOT NULL,
+  `seat_capacity` varchar(100) NOT NULL,
+  `transmission_type` varchar(100) NOT NULL,
+  `boot_space` varchar(100) NOT NULL,
+  `fuel_tank_capacity` varchar(100) NOT NULL,
+  `body_type` varchar(100) NOT NULL,
+  `service_cost` varchar(100) NOT NULL,
+  `power_steering` varchar(100) NOT NULL,
+  `power_window` varchar(100) NOT NULL,
+  `ac` varchar(100) NOT NULL,
+  `driver_airbag` varchar(100) NOT NULL,
+  `passenger_airbag` varchar(100) NOT NULL,
+  `alloy_wheel` varchar(100) NOT NULL,
+  `usr_name` varchar(100) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `usr_phone` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usr_order`
+--
+
+CREATE TABLE `usr_order` (
+  `id` int(100) NOT NULL,
+  `user_id` varchar(100) NOT NULL,
+  `product_id` varchar(100) NOT NULL,
+  `amt` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `add_power`
+--
+ALTER TABLE `add_power`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `add_product`
+--
+ALTER TABLE `add_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `add_spare`
+--
+ALTER TABLE `add_spare`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uname` (`uname`),
+  ADD UNIQUE KEY `emp_id` (`emp_id`);
+
+--
+-- Indexes for table `enquiry`
+--
+ALTER TABLE `enquiry`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `loan`
+--
+ALTER TABLE `loan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_product`
+--
+ALTER TABLE `user_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usr_order`
+--
+ALTER TABLE `usr_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `add_power`
+--
+ALTER TABLE `add_power`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `add_product`
+--
+ALTER TABLE `add_product`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `add_spare`
+--
+ALTER TABLE `add_spare`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(40) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `enquiry`
+--
+ALTER TABLE `enquiry`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loan`
+--
+ALTER TABLE `loan`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_product`
+--
+ALTER TABLE `user_product`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usr_order`
+--
+ALTER TABLE `usr_order`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ user/index.php
@@ -0,0 +1,64 @@
+<?php
+include('header.php');
+?>
+<style>
+    a{color:white;}
+    .bdy
+    {
+        overflow:hidden;
+        background-color:#00000078;
+        margin-top: -20px;
+    }
+    .logo-small {
+    color: #f4511e;
+    font-size: 50px;
+  }
+</style>
+<body class="admin">   
+<div class="bdy">
+   	<div class="row admin_bann">
+    <div class="col-md-6 amca" style="height:530px;background-color:#0000003b;">
+        <h1 class="pull-right" style="margin-top:100px;">
+        <p>"Happy Employees Lead To <br></p>
+        <p>Happy Customers, Which Leads  <br></p>
+        <p>To More Profit.. "<br></p>
+        </h1>
+    </div>
+    <div class="col-md-6" style="height:530px;background-color:#0000003b;"></div>
+    </div>
+    
+    <div id="services" class="container-fluid text-center" style="background-color:#00000087;"><br><br><br>
+  <h2>SERVICES</h2>
+  <br>
+  <div class="row slideanim">
+    <div class="col-md-3">
+      <span class="glyphicon glyphicon-user logo-small"></span>
+      <h4>USER</h4>
+    </div>
+    <div class="col-md-3">
+      <span class="glyphicon glyphicon-user logo-small"></span>
+      <h4>EMPLOYEE</h4>
+    </div>
+    
+  
+    
+    <div class="col-md-3">
+      <span class="glyphicon glyphicon-tasks logo-small"></span>
+      <h4>STOCK</h4>
+    </div>
+    <div class="col-md-3">
+      <span class="glyphicon glyphicon-wrench logo-small"></span>
+      <h4>SERVICE</h4>
+    </div>
+  </div>
+      </div>
+
+    <div class="footer">
+  <p>Footer</p>
+</div>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>    
+</body>
